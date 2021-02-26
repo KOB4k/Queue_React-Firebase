bd80de8cdd441dc0e429773ae01ae630dc8611b8
KOB4k <r.theppakorn@gmail.com>
2021-02-26 23:44:44 +0700

    add firebase config

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from "firebase/app";
 import reportWebVitals from './reportWebVitals';
+const firebaseConfig = {
+  apiKey: "AIzaSyArXkzvZggf7koYXaq8XZp1EDshKYT-lgA",
+  authDomain: "queue-react-firebase.firebaseapp.com",
+  databaseURL: "https://queue-react-firebase-default-rtdb.firebaseio.com",
+  projectId: "queue-react-firebase",
+  storageBucket: "queue-react-firebase.appspot.com",
+  messagingSenderId: "841831499241",
+  appId: "1:841831499241:web:478a3f5409d44585ccb778",
+  measurementId: "G-BJPSKZ2XS6"
+};
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
