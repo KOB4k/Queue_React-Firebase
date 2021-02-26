8f5b1738f2563ad140209eb4ba1e1f3324869fda
KOB4k <r.theppakorn@gmail.com>
2021-02-26 23:45:40 +0700

    update main app to show simple queue

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,8 +4,9 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
 import { Button,Spinner,Badge } from 'react-bootstrap';
-function App() {
-  const [, setIsSignin] = useState(false);
+
+export default function App() {
+  const [signin, setSignin] = useState(false);
   const [serverQueue, setServerQueue] = useState(-1);
   const [myQueue, setMyQueue] = useState(-1);
   const [tempQueue, setTempQueue] = useState(-1);
@@ -20,7 +21,8 @@ function App() {
     });
     firebase.auth().signInAnonymously()
         .then(() => {
-          setIsSignin(true);
+          console.log("sign in");
+          setSignin(true);
         })
         .catch((error) => {
             var errorCode = error.code;
@@ -35,6 +37,8 @@ function App() {
             if(snapshot.exists()){
                 const data = snapshot.val();
                 setMyQueue(data);
+            }else{
+              setMyQueue(-1);
             }
         });
     }
@@ -43,41 +47,37 @@ function App() {
     firebase.database().ref('userQueue/' + firebase.auth().currentUser?.uid).set(tempQueue + 1);
     firebase.database().ref('tempQueue').set(tempQueue + 1);
   };
-  const userArea = () => {
-    myQueue !== -1 && myQueue >= serverQueue?
-        serverQueue !== myQueue ?
-                <div>
-                    <h2>คิวของคุณ คือ <Badge variant="primary">{myQueue}</Badge></h2>
-                    <Badge variant="warning">{`เหลืออีก ${myQueue - serverQueue} คิว`}</Badge>
-                </div>
-                :
-                <div>
-                    <h1><Badge variant="success">ถึงคิวของคุณแล้ว</Badge></h1>
-                </div>
-                :
-            <Button variant="primary" onClick={() => addToQueue()}>
-                เริ่มจองคิว
-            </Button>
-  };
-  const mainApp = () => {
-    myQueue !== -1?
-      <Spinner animation="border" role="status">
-          <span className="sr-only">Loading...</span>
-      </Spinner>
-      :
-      <div>
-          <h1 className="">คิวปัจจุบัน คือ <Badge variant="danger">{serverQueue}</Badge></h1>
-          <hr className="text-white"/>
-          {userArea()}
-      </div>
-  };
   return (
     <div className="App">
       <header className="App-header">
-        {()=>mainApp()}
+        {
+          !signin || serverQueue === -1?
+          <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+          </Spinner>
+          :
+          <div>
+              <h1 className="">คิวปัจจุบัน คือ <Badge variant="danger">{serverQueue}</Badge></h1>
+              <hr className="text-white"/>
+              {
+                myQueue >= serverQueue?
+                serverQueue !== myQueue ?
+                  <div>
+                      <h2>คิวของคุณ คือ <Badge variant="primary">{myQueue}</Badge></h2>
+                      <Badge variant="warning">{`เหลืออีก ${myQueue - serverQueue} คิว`}</Badge>
+                  </div>
+                  :
+                  <div>
+                      <h1><Badge variant="success">ถึงคิวของคุณแล้ว</Badge></h1>
+                  </div>
+                :
+                <Button variant="primary" onClick={() => addToQueue()}>
+                    เริ่มจองคิว
+                </Button>
+              }
+          </div>
+        }
       </header>
     </div>
   );
 }
-
-export default App;
